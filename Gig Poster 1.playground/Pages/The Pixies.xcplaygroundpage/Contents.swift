834fad66d47578b6543d4b3d7228cd4d39408231
Sunaina Vallamkonda <svallamkonda@lcs.on.ca>
2020-01-13 11:20:33 -0500

    created green circles to fill black square

--- Gig Poster 1.playground/Pages/The Pixies.xcplaygroundpage/Contents.swift
+++ Gig Poster 1.playground/Pages/The Pixies.xcplaygroundpage/Contents.swift
@@ -42,11 +42,31 @@ canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
 //change color to black
 canvas.fillColor = Color.black
 canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 400)
+
+//change color to green to see if circles work
+canvas.fillColor = limeGreen
+
+//create circles
+var x = 0
+for columns in 1...11 {
+   //determine x and y positions
+   
+   
+    for column in 0...10 {
+    let y = column * 40
+        //draw circles
+        canvas.drawEllipse(at: Point(x: x, y: y), width: 35, height: 35)
+        if column == 10 {
+            x = x + 40
+        }
+    }
+
+}
 /*:
  ## Use Source Control
  
  Remember to commit and push your work before 11:30 AM on Monday, January 13, 2020, please.
-
+ 
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
  */
